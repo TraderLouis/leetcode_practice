e8f811d5fea7bdcd15515dc87ea7c7371b95f147
TraderLouis <135569359+TraderLouis@users.noreply.github.com>
2025-09-05 23:53:51 +0800

    Delete basic directory

--- basic/Alice_and_Bob_Playing_Flower_Game.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def flowerGame(self, n, m):
-        """
-        :type n: int
-        :type m: int
-        :rtype: int
-        """
-        return ((n/2 + n%2) * (m/2)) + ((m/2 + m%2) * (n/2))
